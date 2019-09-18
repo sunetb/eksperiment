99d6053114aff7ce1415b8cd178e821168c1af31
Sunetb <sunetb@gmail.com>
2019-09-18 15:18:43 +0200

    nr1

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       //Her skriver vi ud til konsollen
+       System.out.println("Skriv et tal"+(2<=3));
+
+
+       Scanner input = new Scanner(System.in);
+       int a = input.nextInt();
+
+       System.out.println("Skriv et tal");
+       int b = input.nextInt();
+        //float f = input.nextFloat();
+       String username = input.nextLine();
+       username = input.nextLine();
+
+       String password = "idfj29nc98ryn2y";
+
+       System.out.println("Hej "+ (a == b));
+       System.out.println("Hej "+ (password.equals(username)));
+
+
+
+       //Er to strenge ens? password.equals(username)
+       input.close();
+
+    }
+}
